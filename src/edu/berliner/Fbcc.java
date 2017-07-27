ee67b7bf0ed848cda640ea736d65109252319732
Jesse <jesseberliner@gmail.com>
2017-07-27 09:51:58 -0400

    Added comments and used better programming practices

--- src/edu/berliner/Fbcc.java
+++ src/edu/berliner/Fbcc.java
@@ -1,6 +1,13 @@
 package edu.berliner;
 
 public class Fbcc {
+	/*
+	 * This applications prints "Fizz" for multiples of 3, "Buzz" for multiples of 5, "Copy" for multiples of 2, "Cat" for multiples of 10, 
+	 * and the combinations for multiples of more than one (ie 15 prints "FizzBuzz", 20 prints "CopyCat", and 30 prints "FizzBuzzCopyCat"
+	 * 
+	 * Creator:Jesse Berliner
+	 * Creation date: 7/27/17
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -36,7 +43,9 @@ public class Fbcc {
 			}
 			else
 			{
-				System.out.println(counter); //otherwise print the number we're on
+				//otherwise print the number we're on
+				//System.out.println(counter);  technically works, most of the time 
+				System.out.println(Integer.toString(counter));  //better practice
 			}
 		}
 	}
